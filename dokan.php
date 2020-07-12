d6c4ae9ae9ec66e35f47ac8475708345dacaf5c9
GP <gp@grouchypenguin.com>
2020-07-12 11:47:27 -0700

    Add minor minor version

--- dokan.php
+++ dokan.php
@@ -3,7 +3,7 @@
 Plugin Name: Dokan
 Plugin URI: https://wordpress.org/plugins/dokan-lite/
 Description: An e-commerce marketplace plugin for WordPress. Powered by WooCommerce and weDevs.
-Version: 3.0.5
+Version: 3.0.5.1
 Author: weDevs
 Author URI: https://wedevs.com/
 Text Domain: dokan-lite
